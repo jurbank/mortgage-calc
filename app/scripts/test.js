00a5b338c1e4e2e78b9ae71cd9cfd3ef7bcc953b
John Urbank <jurbank@fallsdigital.com>
2015-04-10 18:15:46 -0400

    updates

--- app/scripts/test.js
+++ app/scripts/test.js
@@ -1,5 +1,10 @@
 var sliderAmount = $('.slider-amount');
+var sliderIntrest = $('.slider-intrest');
+var sliderTerm = $('.slider-term');
+
 var inputAmount = $('.input-amount'), guard = false;
+var inputIntrest = $('.input-intrest'), guard = false;
+var inputTerm = $('.input-term'), guard = false;
 
 function setSalesValue(value){
     if ( guard ) return;
@@ -7,18 +12,50 @@ function setSalesValue(value){
 }
 
 sliderAmount.noUiSlider({
-    start: [ 0 ],
+    start: [ 90000 ],
     range: {
     'min': [ 0, 1000 ],
     // '33%': [ 1000, 1000 ],
     // '66%': [ 5000, 1000 ],
-    'max': [ 10000000, 1000 ]
+    'max': [ 1000000, 1000 ]
 		},
     format: wNumb({
         decimals: 0,
         mark: '.',
         thousand: ',',
         prefix: '$'
+    })     
+});
+
+sliderIntrest.noUiSlider({
+    start: [ 2 ],
+    range: {
+    'min': [ 0 ],
+    // '33%': [ 1000, 1000 ],
+    // '66%': [ 5000, 1000 ],
+    'max': [ 20, 1000 ]
+		},
+    format: wNumb({
+        decimals: 2,
+        mark: '.',
+        thousand: ',',
+        postfix: '%'
+    })     
+});
+
+sliderTerm.noUiSlider({
+    start: [ 10 ],
+    range: {
+    'min': [ 10, 1000 ],
+    // '33%': [ 1000, 1000 ],
+    // '66%': [ 5000, 1000 ],
+    'max': 30
+		},
+    format: wNumb({
+        // decimals: 2,
+        // mark: '.',
+        thousand: ',',
+        postfix: ' Years'
     })     
 });
 
@@ -30,8 +67,24 @@ sliderAmount.noUiSlider({
 // });
 
 $('.slider-amount').Link("lower").to(inputAmount, setSalesValue);
+$('.slider-intrest').Link("lower").to(inputIntrest, setSalesValue);
+$('.slider-term').Link("lower").to(inputTerm, setSalesValue);
 
 inputAmount.change(function(){
+    var value = $(this).val();
+    guard = true;
+    sliderAmount.val(value);
+    guard = false;
+});
+
+inputIntrest.change(function(){
+    var value = $(this).val();
+    guard = true;
+    sliderAmount.val(value);
+    guard = false;
+});
+
+inputTerm.change(function(){
     var value = $(this).val();
     guard = true;
     sliderAmount.val(value);
